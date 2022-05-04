2126e20c71f0d377f8ff015e9d9d90bfc886c027
Ionut Tripa <ionut.tripa.23@gmail.com>
2022-05-04 22:48:43 +0100

    footer populated

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -3,110 +3,143 @@ const Footer = () => {
 		<footer className="o-footer o-footer--theme-dark" data-o-component="o-footer" data-o-footer--no-js>
 			<div className="o-footer__container">
 				<h2 className="o-footer-visually-hidden">Useful links</h2>
-
 				<div className="o-footer__row">
 					<nav className="o-footer__matrix" role="navigation" aria-label="Useful links">
 						<div className="o-footer__matrix-group o-footer__matrix-group--1">
 							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-0">
-								xxxxxxx
+								Support
 							</h3>
 							<div className="o-footer__matrix-content" id="o-footer-section-0">
 								<div className="o-footer__matrix-column">
-									<a className="o-footer__matrix-link" href="//www.ft.com/help">
-										<span className="o-footer__matrix-link__copy">xxxx</span>
+									<a className="o-footer__matrix-link" href="/tour" data-trackable="View Site Tips">
+										<span className="o-footer__matrix-link__copy">View Site Tips</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//www.ft.com/aboutus">
-										<span className="o-footer__matrix-link__copy">xxxxx xx</span>
+									<a className="o-footer__matrix-link" href="http://help.ft.com" data-trackable="Help Centre">
+										<span className="o-footer__matrix-link__copy">Help Centre</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://aboutus.ft.com/contact-us" data-trackable="Contact Us">
+										<span className="o-footer__matrix-link__copy">Contact Us</span>
+									</a>
+									<a className="o-footer__matrix-link" href="http://www.ft.com/aboutus" data-trackable="About Us"><
+										span className="o-footer__matrix-link__copy">About Us</span>
+									</a>
+									<a className="o-footer__matrix-link" href="/accessibility" data-trackable="Accessibility">
+										<span className="o-footer__matrix-link__copy">Accessibility</span>
+									</a>
+									<a className="o-footer__matrix-link" href="/tour/myft" data-trackable="myFT Tour">
+										<span className="o-footer__matrix-link__copy">myFT Tour</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://aboutus.ft.com/en-gb/careers/" data-trackable="Careers">
+										<span className="o-footer__matrix-link__copy">Careers</span>
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className="o-footer__matrix-group o-footer__matrix-group--1">
-							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-1">
-								xxxxx x xxxxxxx
-							</h3>
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-1">Legal &amp; Privacy</h3>
 							<div className="o-footer__matrix-content" id="o-footer-section-1">
 								<div className="o-footer__matrix-column">
-									<a className="o-footer__matrix-link" href="//www.ft.com/servicestools/help/terms">
-										<span className="o-footer__matrix-link__copy">xxxxx x xxxxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="http://help.ft.com/help/legal-privacy/terms-conditions/" data-trackable="Terms &amp; Conditions">
+										<span className="o-footer__matrix-link__copy">Terms &amp; Conditions</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//www.ft.com/servicestools/help/privacy">
-										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									<a className="o-footer__matrix-link" href="http://help.ft.com/help/legal-privacy/privacy/" data-trackable="Privacy Policy">
+										<span className="o-footer__matrix-link__copy">Privacy Policy</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//www.ft.com/cookiepolicy">
-										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									<a className="o-footer__matrix-link" href="http://help.ft.com/help/legal-privacy/cookies/" data-trackable="Cookies">
+										<span className="o-footer__matrix-link__copy">Cookies</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//www.ft.com/servicestools/help/copyright">
-										<span className="o-footer__matrix-link__copy">xxxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="http://help.ft.com/help/legal-privacy/copyright/copyright-policy/" data-trackable="Copyright">
+										<span className="o-footer__matrix-link__copy">Copyright</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://help.ft.com/help/legal/slavery-statement/" data-trackable="Slavery Statement &amp; Policies">
+										<span className="o-footer__matrix-link__copy">Slavery Statement &amp; Policies</span>
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className="o-footer__matrix-group o-footer__matrix-group--1">
-							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-2">
-								xxxxxxxx
-							</h3>
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-2">Services</h3>
 							<div className="o-footer__matrix-content" id="o-footer-section-2">
 								<div className="o-footer__matrix-column">
-									<a className="o-footer__matrix-link" href="//sub.ft.com/spa_5">
-										<span className="o-footer__matrix-link__copy">xxxxxxxxxx xxxxxxxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="/securedrop" data-trackable="Share News Tips Securely" data-o-tracking-do-not-track="true">
+										<span className="o-footer__matrix-link__copy">Share News Tips Securely</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//enterprise.ft.com/en-gb/services/group-subscriptions/">
-										<span className="o-footer__matrix-link__copy">xxxxx xxxxxxxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="http://www.ft.com/products" data-trackable="Individual Subscriptions">
+										<span className="o-footer__matrix-link__copy">Individual Subscriptions</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//enterprise.ft.com/en-gb/services/republishing/">
-										<span className="o-footer__matrix-link__copy">xxxxxxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://enterprise.ft.com/en-gb/services/group-subscriptions/group-contact-us/?segmentId=383c7f63-abf4-b62d-cb33-4c278e6fdf61&amp;cpccampaign=B2B_link_ft.com_footer" data-trackable="Group Subscriptions">
+										<span className="o-footer__matrix-link__copy">Group Subscriptions</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//www.businessesforsale.com/ft2/notices">
-										<span className="o-footer__matrix-link__copy">xxxxxxxxx x xxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://enterprise.ft.com/en-gb/services/republishing/" data-trackable="Republishing">
+										<span className="o-footer__matrix-link__copy">Republishing</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//commerce.uk.reuters.com/purchase/mostPopular.do?rpc&amp;#x3D;471">
-										<span className="o-footer__matrix-link__copy">xxxxxxxx xxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://www.exec-appointments.com/" data-trackable="Executive Job Search">
+										<span className="o-footer__matrix-link__copy">Executive Job Search</span>
+									</a>
+									<a className="o-footer__matrix-link" href="http://fttoolkit.co.uk/" data-trackable="Advertise with the FT">
+										<span className="o-footer__matrix-link__copy">Advertise with the FT</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://twitter.com/financialtimes" data-trackable="Follow the FT on Twitter">
+										<span className="o-footer__matrix-link__copy">Follow the FT on Twitter</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://transact.ft.com/" data-trackable="FT Transact">
+										<span className="o-footer__matrix-link__copy">FT Transact</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://enterprise.ft.com/en-gb/services/group-subscriptions/secondary-education/" data-trackable="Secondary Schools">
+										<span className="o-footer__matrix-link__copy">Secondary Schools</span>
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className="o-footer__matrix-group o-footer__matrix-group--1">
-							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-3">
-								xxxxx
-							</h3>
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-3">Tools</h3>
 							<div className="o-footer__matrix-content" id="o-footer-section-3">
 								<div className="o-footer__matrix-column">
-									<a className="o-footer__matrix-link" href="//markets.ft.com/data/portfolio/dashboard">
-										<span className="o-footer__matrix-link__copy">xxxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://markets.ft.com/data/portfolio/dashboard" data-trackable="Portfolio">
+										<span className="o-footer__matrix-link__copy">Portfolio</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//ftepaper.ft.com">
-										<span className="o-footer__matrix-link__copy">xxxxxxx xxxxx</span>
+									<a className="o-footer__matrix-link" href="https://www.ft.com/todaysnewspaper" data-trackable="Today's Newspaper (ePaper)">
+										<span className="o-footer__matrix-link__copy">Today&apos;s Newspaper (ePaper)</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//markets.ft.com/data/alerts/">
-										<span className="o-footer__matrix-link__copy">xxxxxx xxx</span>
+									<a className="o-footer__matrix-link" href="http://markets.ft.com/data/alerts/" data-trackable="Alerts Hub">
+										<span className="o-footer__matrix-link__copy">Alerts Hub</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//lexicon.ft.com/">
-										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://rankings.ft.com/" data-trackable="Business School Rankings">
+										<span className="o-footer__matrix-link__copy">Business School Rankings</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//rankings.ft.com/businessschoolrankings/global-mba-ranking-2016">
-										<span className="o-footer__matrix-link__copy">xxx xxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://kat.ft.com/" data-trackable="Enterprise Tools">
+										<span className="o-footer__matrix-link__copy">Enterprise Tools</span>
+									</a>
+									<a className="o-footer__matrix-link" href="/news-feed" data-trackable="News feed">
+										<span className="o-footer__matrix-link__copy">News feed</span>
+									</a>
+									<a className="o-footer__matrix-link" href="/newsletters" data-trackable="Newsletters">
+										<span className="o-footer__matrix-link__copy">Newsletters</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://markets.ft.com/research/Markets/Currencies?segid=70113" data-trackable="Currency Converter">
+										<span className="o-footer__matrix-link__copy">Currency Converter</span>
 									</a>
 								</div>
 							</div>
 						</div>
 						<div className="o-footer__matrix-group o-footer__matrix-group--1">
-							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-4">
-								xxxxxxx
-							</h3>
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-4">Community &amp; Events</h3>
 							<div className="o-footer__matrix-content" id="o-footer-section-4">
 								<div className="o-footer__matrix-column">
-									<a className="o-footer__matrix-link" href="//markets.ft.com/data/portfolio/dashboard">
-										<span className="o-footer__matrix-link__copy">xxxxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://www.ft.com/tour/community" data-trackable="FT Community">
+										<span className="o-footer__matrix-link__copy">FT Community</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//ftepaper.ft.com">
-										<span className="o-footer__matrix-link__copy">xxxxxxx xxxxx</span>
+									<a className="o-footer__matrix-link" href="http://live.ft.com/" data-trackable="FT Live">
+										<span className="o-footer__matrix-link__copy">FT Live</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//markets.ft.com/data/alerts/">
-										<span className="o-footer__matrix-link__copy">xxxxxx xxx</span>
+									<a className="o-footer__matrix-link" href="https://forums.ft.com/" data-trackable="FT Forums">
+										<span className="o-footer__matrix-link__copy">FT Forums</span>
 									</a>
-									<a className="o-footer__matrix-link" href="//lexicon.ft.com/">
-										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									<a className="o-footer__matrix-link" href="https://enterprise.ft.com/board-director-membership" data-trackable="FT Board Director">
+										<span className="o-footer__matrix-link__copy">FT Board Director</span>
+									</a>
+									<a className="o-footer__matrix-link" href="https://bdp.ft.com/" data-trackable="Board Director Programme">
+										<span className="o-footer__matrix-link__copy">Board Director Programme</span>
 									</a>
 								</div>
 							</div>
