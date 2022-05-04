b8808e8e82752345998dcf15eb75a506767d63aa
Ionut Tripa <ionut.tripa.23@gmail.com>
2022-05-04 01:00:20 +0100

    creates footer and header components/need populatin

---
+++ Components/Footer/Footer.js
@@ -0,0 +1,145 @@
+const Footer = () => {
+	return (
+		<footer className="o-footer o-footer--theme-dark" data-o-component="o-footer" data-o-footer--no-js>
+			<div className="o-footer__container">
+				<h2 className="o-footer-visually-hidden">Useful links</h2>
+
+				<div className="o-footer__row">
+					<nav className="o-footer__matrix" role="navigation" aria-label="Useful links">
+						<div className="o-footer__matrix-group o-footer__matrix-group--1">
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-0">
+								xxxxxxx
+							</h3>
+							<div className="o-footer__matrix-content" id="o-footer-section-0">
+								<div className="o-footer__matrix-column">
+									<a className="o-footer__matrix-link" href="//www.ft.com/help">
+										<span className="o-footer__matrix-link__copy">xxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//www.ft.com/aboutus">
+										<span className="o-footer__matrix-link__copy">xxxxx xx</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="o-footer__matrix-group o-footer__matrix-group--1">
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-1">
+								xxxxx x xxxxxxx
+							</h3>
+							<div className="o-footer__matrix-content" id="o-footer-section-1">
+								<div className="o-footer__matrix-column">
+									<a className="o-footer__matrix-link" href="//www.ft.com/servicestools/help/terms">
+										<span className="o-footer__matrix-link__copy">xxxxx x xxxxxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//www.ft.com/servicestools/help/privacy">
+										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//www.ft.com/cookiepolicy">
+										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//www.ft.com/servicestools/help/copyright">
+										<span className="o-footer__matrix-link__copy">xxxxxxxxx</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="o-footer__matrix-group o-footer__matrix-group--1">
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-2">
+								xxxxxxxx
+							</h3>
+							<div className="o-footer__matrix-content" id="o-footer-section-2">
+								<div className="o-footer__matrix-column">
+									<a className="o-footer__matrix-link" href="//sub.ft.com/spa_5">
+										<span className="o-footer__matrix-link__copy">xxxxxxxxxx xxxxxxxxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//enterprise.ft.com/en-gb/services/group-subscriptions/">
+										<span className="o-footer__matrix-link__copy">xxxxx xxxxxxxxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//enterprise.ft.com/en-gb/services/republishing/">
+										<span className="o-footer__matrix-link__copy">xxxxxxxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//www.businessesforsale.com/ft2/notices">
+										<span className="o-footer__matrix-link__copy">xxxxxxxxx x xxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//commerce.uk.reuters.com/purchase/mostPopular.do?rpc&amp;#x3D;471">
+										<span className="o-footer__matrix-link__copy">xxxxxxxx xxxxxxxx</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="o-footer__matrix-group o-footer__matrix-group--1">
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-3">
+								xxxxx
+							</h3>
+							<div className="o-footer__matrix-content" id="o-footer-section-3">
+								<div className="o-footer__matrix-column">
+									<a className="o-footer__matrix-link" href="//markets.ft.com/data/portfolio/dashboard">
+										<span className="o-footer__matrix-link__copy">xxxxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//ftepaper.ft.com">
+										<span className="o-footer__matrix-link__copy">xxxxxxx xxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//markets.ft.com/data/alerts/">
+										<span className="o-footer__matrix-link__copy">xxxxxx xxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//lexicon.ft.com/">
+										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//rankings.ft.com/businessschoolrankings/global-mba-ranking-2016">
+										<span className="o-footer__matrix-link__copy">xxx xxxxxxxx</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="o-footer__matrix-group o-footer__matrix-group--1">
+							<h3 className="o-footer__matrix-title" aria-controls="o-footer-section-4">
+								xxxxxxx
+							</h3>
+							<div className="o-footer__matrix-content" id="o-footer-section-4">
+								<div className="o-footer__matrix-column">
+									<a className="o-footer__matrix-link" href="//markets.ft.com/data/portfolio/dashboard">
+										<span className="o-footer__matrix-link__copy">xxxxxxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//ftepaper.ft.com">
+										<span className="o-footer__matrix-link__copy">xxxxxxx xxxxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//markets.ft.com/data/alerts/">
+										<span className="o-footer__matrix-link__copy">xxxxxx xxx</span>
+									</a>
+									<a className="o-footer__matrix-link" href="//lexicon.ft.com/">
+										<span className="o-footer__matrix-link__copy">xxxxxxx</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="o-footer__matrix-group o-footer__matrix-group--1">
+							<h3 className="o-footer__matrix-title o-footer__matrix-title--link">
+								<a className="o-footer__matrix-link o-footer__matrix-link--more" id="o-footer-section-5" href="https://ft.com/more-from-ft-group">
+									<span className="o-footer__matrix-link__copy">More from the FT Group</span>
+								</a>
+							</h3>
+						</div>
+					</nav>
+				</div>
+
+				<div className="o-footer__copyright">
+					<small>
+						Markets data delayed by at least 15 minutes. &#xA9; THE FINANCIAL
+						TIMES LTD 2021.
+						<abbr title="Financial Times" aria-label="F T">FT</abbr> and
+						&#x2018;Financial Times&#x2019; are trademarks of The Financial Times Ltd.<br />
+						The Financial Times and its journalism are subject to a
+						self-regulation regime under the
+						<a href="http://aboutus.ft.com/en-gb/ft-editorial-code/" aria-label="F T Editorial Code of Practice">FT Editorial Code of Practice</a>.
+					</small>
+				</div>
+			</div>
+			<div className="o-footer__brand">
+				<div className="o-footer__container">
+					<div className="o-footer__brand-logo"></div>
+				</div>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link rel="icon" type="image/svg+xml" href="https://www.ft.com/__origami/service/image/v2/images/raw/ftlogo-v1%3Abrand-ft-logo-square-coloured?source=update-logos&amp;format=svg"></link>
+				<link rel="stylesheet" href="https://www.ft.com/__origami/service/build/v3/bundles/css?components=o-header@^10.0.1,o-footer@^9.2.2,o-colors@^6.0.8,o-typography@^7.0.2&brand=core&system_code=$$$-no-bizops-system-code-$$$" />
+				<script src="https://www.ft.com/__origami/service/build/v3/bundles/js?components=o-header@^10.0.1,o-autoinit@^3.0.0,o-footer@^9.2.2,o-colors@^6.0.8,o-typography@^7.0.2&system_code=$$$-no-bizops-system-code-$$$" defer></script>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
